6cd00c885773d58457910fa0ffdae0bdab67059f
PMFarms <manohar781996@gmail.com>
2025-04-25 22:47:35 +0530

    added

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors()); // Allow cross-origin requests
 //     database: "student_db"
 // });
 const db = mysql.createConnection({
-    host: "mysql",
+    host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
